868e3a9ecd2f6f5002c90f34a7d092aeff9ae5ef
Maikkoum <Blueddo@users.noreply.github.com>
2024-10-23 21:45:28 +0300

    Add files via upload

--- Check_tiktok_live.py
+++ Check_tiktok_live.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import subprocess
+import os
 from datetime import datetime, timedelta
 
 def load_users():
@@ -28,7 +29,6 @@ def check_user(user):
     return user, None, None
 
 def update_m3u_file(live_users):
-    profile_data = load_profile_data()
     default_stream_url = "https://ak5.picdn.net/shutterstock/videos/1072225/preview/stock-footage-pretty-lady-watching-tv-with-popcorn-in-her-living-room.mp4"
     try:
         with open("tiktok_live.m3u", "w", encoding="utf-8") as m3u_file:
@@ -81,14 +81,35 @@ def get_avatar_thumb(user, profile_data):
 def check_image_access(url):
     try:
         response = requests.get(url)
-        response.raise_for_status()  # Βεβαιωθείτε ότι το αίτημα ήταν επιτυχές
+        response.raise_for_status()
         return True
     except requests.RequestException:
         return False
 
+def push_to_github():
+    try:
+        # Configure Git user with a no-reply email
+        subprocess.run(["git", "config", "--global", "user.email", "Blueddo@users.noreply.github.com"], check=True)
+        subprocess.run(["git", "config", "--global", "user.name", "Blueddo"], check=True)
+
+        # Add changes to git
+        subprocess.run(["git", "add", "tiktok_live.m3u"], check=True)
+        subprocess.run(["git", "commit", "-m", "Update tiktok_live.m3u"], check=True)
+
+        # Push changes to GitHub using PAT
+        subprocess.run(["git", "push", "https://Blueddo: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@github.com/Blueddo/tiktok-live-script.git"], check=True)
+        print("Αλλαγές ανέβηκαν στο GitHub.")
+    except subprocess.CalledProcessError as e:
+        print(f"Σφάλμα κατά την προώθηση αλλαγών στο GitHub: {e}")
+
+# Στο τέλος της συνάρτησης fetch_and_save_data, προσθέστε αυτήν την κλήση:
+push_to_github()
+
+
 def fetch_and_save_data():
     users = load_users()
     live_users = {}
+    push_to_github()
 
     for user in users:
         user, output, start_time = check_user(user)
